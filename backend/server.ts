1d3f94884d5daad70769eb6f87a90dc0c010fd0d
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-21 10:14:17 +0530

    cors type error fixed

--- backend/server.ts
+++ backend/server.ts
@@ -8,9 +8,11 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get('/',(req: express.Request, res: express.Response) => {
     res.send('Hello, World!');
 })
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
@@ -37,21 +39,4 @@ app.listen(PORT, () => {
 
 
 
-
-// import { OpenRouter } from '@openrouter/sdk';
-
-// const client = new OpenRouter({
-//   apiKey: process.env.OPENROUTER_API_KEY,
-// });
-
-// const response = await client.chat.send({
-//     chatRequest: {
-//   model: 'openai/gpt-5.2',
-//   messages: [
-//     { role: 'user', content: 'Explain quantum computing in one sentence.' },
-//   ],
-// }
-// });
-
-// console.log(response)
 
