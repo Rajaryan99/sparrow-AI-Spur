b68215ed1fa273361c6bbd1141cc0f03a2e0b50e
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-25 13:06:56 +0530

    side bar logo and new chat button added

--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './sidebar.css'
+import  icon from '../../assets/favicon.png'
 
 export default function SideBar() {
   return (
-    <div>SideBar</div>
+    <section className='sidebar'>
+
+
+      <div className='logo' >
+        <img src={icon} className='icon' alt="" />
+        <h2>AI Assistent</h2>
+      </div>
+     
+
+      <ul className='history'>
+        <li>nkdj,</li>
+        <li>ndkl</li>
+        <li>lkdj</li>
+        <li>nclkd</li>
+      </ul>
+
+ <button className='newChatBtn'>
+              <h3>New Chat</h3> 
+     </button>
+
+    </section>
   )
 }
