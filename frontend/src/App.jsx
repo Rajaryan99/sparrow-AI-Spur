57ef3ad05e382916cbe5ab1b145b34ddd62685a2
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-26 10:00:55 +0530

    fetching AI response from backedm to fromtemd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react'
 import SideBar from './components/sidebar/SideBar'
 import ChatWindow from './components/chatWindow/ChatWindow'
 import { myContext } from './Context'
+import {v4 as uuidv4} from 'uuid'
 
 import './App.css'
 
 function App() {
-  const providerValues = {};
+
+  const [prompt, setPrompt] = useState("")
+  const [reply, setReply] = useState(null)
+  const [currThreadId, setCurrThreadId] = useState(uuidv4())
+
+  const providerValues = {
+    prompt, setPrompt,
+    reply, setReply,
+    currThreadId, setCurrThreadId
+  };
 
   return (
     <>
--- frontend/src/components/chatWindow/ChatWindow.jsx
+++ frontend/src/components/chatWindow/ChatWindow.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './chatwindow.css'
-import chat from '../chat/Chat'
 import Chat from '../chat/Chat'
+import { myContext } from '../../Context'
 
 export default function ChatWindow() {
+
+  const {prompt, setPrompt, reply, setReply, currThreadId} = useContext(myContext)
+
+  const getReply = async () => {
+
+    console.log("Message", prompt, "threadId", currThreadId)
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: prompt,
+        threadId: currThreadId
+      })
+    }
+
+    try {
+
+      const response = await fetch("http://localhost:3000/api/chat", options)
+      console.log(response)
+      
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
     <div className="chatwindow">
@@ -16,8 +43,13 @@ export default function ChatWindow() {
 
       <div className="chatInput">
           <div className='userInput'>
-            <input type="text" placeholder='How can i help you' />
-            <div className='sendBtn'><i class="fa-solid fa-paper-plane"></i></div>
+            <input 
+                type="text" 
+                value={prompt} 
+                onChange={(e) => setPrompt(e.target.value)}
+                placeholder='How can i help you' 
+              />
+            <div className='sendBtn' onClick={getReply}><i className="fa-solid fa-paper-plane"></i></div>
           </div>
           <p className='info'>
 Welcome to Sparrow, your AI-powered assistant for SPUR. Ask any question, and I'll be happy to help.          </p>
