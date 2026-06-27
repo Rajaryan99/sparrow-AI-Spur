8e02ada34d877d4389202cc87b9ee1f2c4765e62
Rajaryan99 <raj.aryan2518@gmail.com>
2026-06-27 22:18:15 +0530

    added a typing effect for AI reply

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './chat.css'
 import { myContext } from '../../Context'
 import ReactMarkdown from 'react-markdown'
@@ -8,13 +8,35 @@ export default function Chat() {
 
 
   const {reply, prevChats, setPrevChats, newChat, setNewChat} = useContext(myContext)
+  const [latestReply, setLatestReply] = useState(null)
+
+
+  useEffect(() => {
+
+    if(!prevChats?.length) return;
+
+    const content = reply.split(" ");
+
+    let idx = 0;
+    const intervel = setInterval(() => {
+      setLatestReply(content.slice(0, idx + 1).join(" "))
+
+      idx++;
+      if(idx >= content.length) clearInterval(intervel)
+
+    }, 40)
+
+    return () => clearInterval(intervel)
+
+  }, [prevChats, reply])
+
   return (
     <>
     {newChat && <h1>Ask your query???</h1>}
     <div className='chatBox'>
 
       {
-        prevChats?.map((chat, idx) => 
+        prevChats?.slice(0, -1).map((chat, idx) => 
           <div className={chat.role === "user"? "prompt": "AIReply"} key={idx}>
                   {
                     chat.role === "user"? 
@@ -24,6 +46,17 @@ export default function Chat() {
           </div>
 
         )
+      }
+
+
+      {
+
+        prevChats.length > 0 && latestReply !== null && 
+        <div className="AIReply" key={"typing"}>
+               <ReactMarkdown>{latestReply}</ReactMarkdown>
+        </div>
+
+
       }
 
 
